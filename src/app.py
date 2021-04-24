daa9e392449ce3b5c500a745350c600637496d5a
pgadgul <poojagadgul@gmail.com>
2021-04-24 04:03:32 -0400

    Updated app

--- src/app.py
+++ src/app.py
@@ -8,10 +8,17 @@ import plotly.graph_objects as go
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
+@app.route("/")
+def index():
+    return "Hello,world!"
 
 df = pd.read_csv('2020-01-28T10_37_27-05_00_SEN_Hk_tlm_t.csv')
 
+# remove rows if the column value is out of range[set_minimum, set_maximum]
+def remove_outliers(df,column_string,set_maximum,set_minimum):
+        dataframe = df[df[column_string] > set_minimum]
+        dataframe = dataframe[dataframe[column_string] < set_maximum]
+        return dataframe
 df_temp = remove_outliers(df,'TEMP_0',500,-500)
 TEMP_0_ro = df_temp['TEMP_0'].values.tolist()
 fig=plt.plot(TEMP_0_ro)
@@ -34,8 +41,3 @@ app.layout = html.Div([
 if __name__ == '__main__': 
     app.run_server(debug=True)
 
-# remove rows if the column value is out of range[set_minimum, set_maximum]
-def remove_outliers(df,column_string,set_maximum,set_minimum):
-        dataframe = df[df[column_string] > set_minimum]
-        dataframe = dataframe[dataframe[column_string] < set_maximum]
-        return dataframe
