55bfa6e7ae2b8d51a023dea73e1c597ddfd82cb5
pgadgul <poojagadgul@gmail.com>
2021-04-24 03:42:36 -0400

    check app connection

--- src/app.py
+++ src/app.py
@@ -5,97 +5,37 @@ from dash.dependencies import Input, Output
 
 import requests 
 import plotly.graph_objects as go
+import matplotlib.pyplot as plt
 import pandas as pd
 
-pd.set_option("display.max_rows", 999)
-pd.set_option("display.max_columns", 999)
 
-df = pd.read_csv('WHS8_110.csv')
 
-new_columns = {
-    'Country' : 'country'
-}
+df = pd.read_csv('2020-01-28T10_37_27-05_00_SEN_Hk_tlm_t.csv')
 
-df.columns = df.iloc[0].values
-df = df[1:]
-df = df.fillna(0).copy()
-df.columns = df.columns.astype(str)
-df.columns = [col.replace('.0','') for col in df.columns]
-df.rename(columns=new_columns, inplace=True)
-
-#df_country = df.head(30)
-#df_year = df.head(30)
+df_temp = remove_outliers(df,'TEMP_0',500,-500)
+TEMP_0_ro = df_temp['TEMP_0'].values.tolist()
+fig=plt.plot(TEMP_0_ro)
 
 app = dash.Dash() 
 
 server = app.server
 
-app.layout = html.Div(
-    [ 
-        html.H1(
-            'Immunization Dashboard',
-            style = {'text-align' : 'center'}
-        ), 
-        html.Div([ 
-            dcc.Dropdown(
-                id='year_selection',
-                options=[
-                    {'label': '2019', 'value': '2019'},
-                    {'label': '2018', 'value': '2018'},
-                    {'label': '2017', 'value': '2017'},
-                    {'label': '2016', 'value': '2016'},
-                    {'label': '2015', 'value': '2015'},
-                    {'label': '2014', 'value': '2014'},
-                    {'label': '2013', 'value': '2013'},
-                    {'label': '2012', 'value': '2012'}
-                ],
-                value = '2019'
-            ),
-            dcc.Graph(id ='bargraph'),
-            dcc.Dropdown(
-                id='country_selection',
-                options=[
-                    {'label': 'Romania', 'value': 'Romania'},
-                    {'label': 'United States of America', 'value': 'United States of America'},
-                    {'label': 'France', 'value': 'France'},
-                    {'label': 'Hungary', 'value': 'Hungary'},
-                    {'label': 'Italy', 'value': 'Italy'},
-                    {'label': 'Sudan', 'value': 'Sudan'},
-                    {'label': 'Spain', 'value': 'Spain'},
-                    {'label': 'Egypt', 'value': 'Egypt'},
-                    {'label': 'India', 'value': 'India'},
-                ],
-                value = 'Romania'
-            ),
-            dcc.Graph(id ='linegraph'),
-        ],
-    style= {'padding':10})
-    ]
-)
+app.layout = html.Div([
+    dcc.Graph(id="graph", figure=fig),
+    html.A(
+        html.Button("Download HTML"), 
+        id="download",
+        href="data:text/html;base64," + encoded,
+        download="plotly_graph.html"
+    )
+])
 
-@app.callback(
-    Output('bargraph','figure'),         
-    [Input('year_selection','value')]
-)
-def retrieve_revenue(year):
-    df_country = df['country'].head(20)
-    df_year = df[year].head(20)
-    datapoints = {'data': [go.Bar(x=df_country, y=df_year)],'layout': dict(yaxis={'title':'Vaccination %'}, )} 
-    return datapoints
- 
-@app.callback(
-    Output('linegraph','figure'), 
-    [Input('country_selection','value')]
-) 
-def retrieve_revenue(country): 
-    df_country = df[df['country'] == country]
-    df_country = df_country.T.drop('country', axis=0)
-    df_country.columns = ['rate']
-    df_country = df_country.rename(columns={df_country.columns[0] : 'rate'})
-    years = list(df_country.index)
-    rates = list(df_country['rate'].values)
-    datapoints = {'data': [go.Scatter(x=years, y=rates, mode="lines+markers")], 'layout' : dict(yaxis=dict(range=[0,100]))}
-    return datapoints 
 
 if __name__ == '__main__': 
     app.run_server(debug=True)
+
+# remove rows if the column value is out of range[set_minimum, set_maximum]
+def remove_outliers(df,column_string,set_maximum,set_minimum):
+        dataframe = df[df[column_string] > set_minimum]
+        dataframe = dataframe[dataframe[column_string] < set_maximum]
+        return dataframe
